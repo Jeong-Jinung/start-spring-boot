bc1e8e7dad7d88f375569fd5c563100a64509038
DevJeong <jinung.dev@gmail.com>
2020-09-06 22:17:54 +0900

    스프링 JPA 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,29 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-  private EntityManager em;
+  private final MemberRepository memberRepository;
 
   @Autowired
-  public SpringConfig(EntityManager em) {
-    this.em = em;
+  public SpringConfig(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
   }
 
   @Bean
   public MemberService memberService() {
-    return new MemberService(memberRepository());
+    return new MemberService(memberRepository);
   }
 
-  @Bean
-  public MemberRepository memberRepository() {
-//    return new MemoryMemberRepository();
-//    return new JdbcMemberRepository(dataSource);
-//    return new jdbcTemplateMemberRepository(dataSource);
-    return new JpaMemberRepository(em);
-  }
+//  @Bean
+//  public MemberRepository memberRepository() {
+////    return new MemoryMemberRepository();
+////    return new JdbcMemberRepository(dataSource);
+////    return new jdbcTemplateMemberRepository(dataSource);
+//    return new JpaMemberRepository(em);
+//  }
 }
